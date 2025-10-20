6b7aa92e3719ba58a86484197a304b3de351160d
Bruno Krugel <brunokrugel@gmail.com>
2025-10-19 22:55:29 -0300

    Add containers and workflows

--- cmd/sample-release-app/main.go
+++ cmd/sample-release-app/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	cfg := zapcore.EncoderConfig{
+		TimeKey:          "dt",
+		LevelKey:         "level",
+		MessageKey:       "message",
+		CallerKey:        "caller",
+		ConsoleSeparator: " ",
+		EncodeLevel:      zapcore.LowercaseLevelEncoder,
+		EncodeTime:       zapcore.RFC3339NanoTimeEncoder,
+		EncodeCaller:     zapcore.ShortCallerEncoder,
+	}
+
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(cfg),
+		zapcore.AddSync(os.Stdout),
+		zap.InfoLevel,
+	)
+
+	logger := zap.New(core,
+		zap.AddCaller(),
+		zap.AddCallerSkip(1),
+	)
+
+	logger.Info("Hello, World!")
 }
